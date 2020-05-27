e18111401970cb4ece53d73623eb8f83888c2a2f
Coder-Iro <doohee006@gmail.com>
2020-05-27 22:32:42 +0900

    Add getting raw timetable data feature

--- comcigan/school.py
+++ comcigan/school.py
@@ -9,6 +9,7 @@ BASEURL = "http://comci.kr:4082/191401?"
 class School:
     name: str
     sccode: int
+    _timeurl: str
 
     def __init__(self, name):
         scsearch = requests.get(BASEURL + "80670l" + "%".join(str(name.encode("EUC-KR")).upper()[2:-1]
@@ -22,4 +23,10 @@ class School:
         else:
             self.name = sclist[0][2]
             self.sccode = sclist[0][3]
-        timeurl = BASEURL + base64.b64encode(f"54952_{self.sccode}_0_1".encode("UTF-8"))
+        self._timeurl = BASEURL + base64.b64encode(f"54952_{self.sccode}_0_1".encode("UTF-8"))
+
+    def refresh(self):
+        time_res = requests.get(self._timeurl)
+        time_res.encoding = "UTF-8"
+        rawtimetable = json.loads(time_res.text.replace("\0", ""))
+
