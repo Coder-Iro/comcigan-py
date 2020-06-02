6a11ae9267229684586e6a1a735cc00ea2b63aa7
Coder-Iro <doohee006@gmail.com>
2020-06-02 20:53:35 +0900

    Edited error of PREFIX regex
    PREFIX number can be 5 or 6 digits

--- comcigan/reg.py
+++ comcigan/reg.py
@@ -1,7 +1,7 @@
 import re
 
 routereg: re.Pattern = re.compile(r'\./\d{6}\?\d{5}l')
-prefixreg: re.Pattern = re.compile(r'\d{6}_')
+prefixreg: re.Pattern = re.compile(r"'\d{5,6}_'")
 orgdatareg: re.Pattern = re.compile(r'원자료=자료\.자료\d{3}')
 daydatareg: re.Pattern = re.compile(r'일일자료=자료\.자료\d{3}')
 thnamereg: re.Pattern = re.compile(r'성명=자료\.자료\d{3}')
