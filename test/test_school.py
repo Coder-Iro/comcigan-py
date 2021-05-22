56abf5c196c6e7d733ec5b3305978cebe96dbed3
kiki7000 <devkiki7000@gmail.com>
2021-05-22 17:28:25 +0900

    test: asyncschooltestcase

--- test/test_school.py
+++ test/test_school.py
@@ -1,9 +1,12 @@
 import unittest
+import asyncio
 
-from comcigan import School
+from comcigan import School, AsyncSchool
 from collections.abc import Iterable
 
 school = School("운정고등학교")
+asyncschool = asyncio.get_event_loop().run_until_complete(AsyncSchool.init("운정고등학교"))
+
 
 class SchoolTestCase(unittest.TestCase):
     def test_getitem(self):
@@ -11,12 +14,27 @@ class SchoolTestCase(unittest.TestCase):
 
     def test_repr(self):
         self.assertIsInstance(repr(school), str)
-    
+
     def test_str(self):
         self.assertIsInstance(str(school), str)
-    
+
     def test_iter(self):
         self.assertIsInstance(iter(school), Iterable)
+
+
+class AsyncSchoolTestCase(unittest.TestCase):
+    def test_getitem(self):
+        self.assertIsInstance(asyncschool[0], list)
+
+    def test_repr(self):
+        self.assertIsInstance(repr(asyncschool), str)
+
+    def test_str(self):
+        self.assertIsInstance(str(asyncschool), str)
+
+    def test_iter(self):
+        self.assertIsInstance(iter(asyncschool), Iterable)
+
 
 if __name__ == "__main__":
     unittest.main()
